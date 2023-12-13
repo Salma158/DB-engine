4a700d99d862555e1126059b8f2ba2accec78d83
Omnia-Mohamed88 <omnia.mohammad.ahmedd@gmail.com>
2023-12-13 09:37:10 +0200

    modified the create_db as if there is no dir called data it will creat it then create the data base in side it

--- create_db.sh
+++ create_db.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-while true
-do
+data_folder="./data"
+
+# Check if the "data" folder exists
+if [ ! -d "$data_folder" ]; then
+    mkdir "$data_folder"
+    echo "Created 'data' folder."
+fi
+
+while true; do
     echo "Please enter a valid database name (without spaces or special characters):"
     IFS= read -r dbname
-
-    # Replace spaces with underscores
-    dbname="${dbname// /_}"
 
     case $dbname in
         '' )
             echo "Error: You entered an empty name. Please enter a proper name."
+            continue;;
+        ' '*)  # Check if the name starts with a space
+            echo "Error: The name cannot start with a space."
+            continue;;
+        *' ' | ' '* | *' '* )
+            echo "Error: The name cannot start, end, or contain spaces in between."
             continue;;
         [0-9]* )
             echo "Error: The name should not start with numbers."
@@ -20,11 +30,11 @@ do
             continue;;
         * )
             # Check if the database already exists in the ./data folder
-            dbpath="./data/$dbname"
-            if [ -e "$dbpath" ] && [ -d "$dbpath" ]; then
-                echo "Error: Database '$dbname' already exists in ./data."
+            dbpath="$data_folder/$dbname"
+            if [ -n "$(find "$data_folder" -type d -name "$dbname" -mindepth 1 -maxdepth 1 2>/dev/null)" ]; then
+                echo "Error: Database '$dbname' already exists in $data_folder."
                 echo "Existing databases:"
-                ls -1 "./data"
+                ls -1 "$data_folder"
                 continue
             else
                 mkdir -p "$dbpath" && echo "Success: Database directory '$dbname' created."
@@ -33,6 +43,7 @@ do
             ;;
     esac
 done
+
 
 
 
